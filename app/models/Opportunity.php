c3f9a65f8bd69b135b2c6be1dad880b0a7066dd3
Mazsi77 <zsoltszekely8@gmail.com>
2021-08-06 23:32:40 +0300

    Zsombi's files

--- app/models/Opportunity.php
+++ app/models/Opportunity.php
@@ -18,12 +18,12 @@ class Opportunity{
         $this->db->query('INSERT INTO opportunities(lead_id, stage_id, name,amount,prob,close-date) VALUES(:lead_id, :stage_id,:name,:amount,:prob, :close-date');
 
 
-        $this->db-bind(":lead_id", $data['lead_id']);
-        $this->db-bind(":stage_id", $data['stage_id']);
-        $this->db-bind(":name", $data['name']);
-        $this->db-bind(":amount", $data['email']);
-        $this->db-bind(":prob", $data['prob']);
-        $this->db-bind(":close-date", $data['close-date']);
+        $this->db->bind(":lead_id", $data['lead_id']);
+        $this->db->bind(":stage_id", $data['stage_id']);
+        $this->db->bind(":name", $data['name']);
+        $this->db->bind(":amount", $data['email']);
+        $this->db->bind(":prob", $data['prob']);
+        $this->db->bind(":close-date", $data['close-date']);
 
         if($this->db->execute()){
             return true;
