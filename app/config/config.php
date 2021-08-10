1f2fa871fd07e3c73405bf1a0351e5f9c262806d
Mazsi77 <zsoltszekely8@gmail.com>
2021-08-10 20:13:06 +0300

    Reseted the root forlder to MiniCrm

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
     //Database params
     define('DB_HOST', 'localhost'); //Add your db host
-    define('DB_USER', 'shaun'); // Add your DB root
-    define('DB_PASS', 'test1234'); //Add your DB pass
+    define('DB_USER', 'root'); // Add your DB root
+    define('DB_PASS', ''); //Add your DB pass
     define('DB_NAME', 'minicrm'); //Add your DB Name
 
     //APPROOT
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URLROOT (Dynamic links)
-    define('URLROOT', 'http://localhost/project');
+    define('URLROOT', 'http://localhost/MiniCrm');
 
     //Sitename
     define('SITENAME', 'MiniCRM');
