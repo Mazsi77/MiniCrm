8b8fdbecc0ab3b693242d2a2a536433e2090aaa5
A-Zsombi <87830919+A-Zsombi@users.noreply.github.com>
2021-08-16 14:00:11 +0300

    Added Stacked Bar Chart

--- app/controllers/Dashboards.php
+++ app/controllers/Dashboards.php
@@ -20,8 +20,46 @@ class Dashboards extends Controller{
 			$data4= $this->dashboardModel->getCountNegotiation();
 			$data5= $this->dashboardModel->getCountFinished();
 			$data6= $this->dashboardModel->getCountNotFinished();
-
-			$this->view('dashboards/displayDashboards', $data, $datas, $data1, $data2, $data3, $data4, $data5, $data6);
+			$janWon= $this->dashboardModel->getjanWon();
+		    $janLost= $this->dashboardModel->getjanLost();
+			$janPros= $this->dashboardModel->getjanPros();
+			$janNeg= $this->dashboardModel->getjanNeg();
+			$febWon= $this->dashboardModel->getfebWon();
+			$febLost= $this->dashboardModel->getfebLost();
+			$febPros= $this->dashboardModel->getfebPros();
+			$febNeg= $this->dashboardModel->getfebNeg();
+			$marWon= $this->dashboardModel->getmarWon();
+			$marLost= $this->dashboardModel->getmarLost();
+			$marPros= $this->dashboardModel->getmarPros();
+			$marNeg= $this->dashboardModel->getmarNeg();
+			$aprWon= $this->dashboardModel->getaprWon();
+			$aprLost= $this->dashboardModel->getaprLost();
+			$aprPros= $this->dashboardModel->getaprPros();
+			$aprNeg= $this->dashboardModel->getaprNeg();
+			$mayWon= $this->dashboardModel->getmayWon();
+			$mayLost= $this->dashboardModel->getmayLost();
+			$mayPros= $this->dashboardModel->getmayPros();
+			$mayNeg= $this->dashboardModel->getmayNeg();
+			$junWon= $this->dashboardModel->getjunWon();
+			$junLost= $this->dashboardModel->getjunLost();
+			$junPros= $this->dashboardModel->getjunPros();
+			$junNeg= $this->dashboardModel->getjunNeg();
+			$julWon= $this->dashboardModel->getjulWon();
+			$julLost= $this->dashboardModel->getjulLost();
+			$julPros= $this->dashboardModel->getjulPros();
+			$julNeg= $this->dashboardModel->getjulNeg();
+			$augWon= $this->dashboardModel->getaugWon();
+			$augLost= $this->dashboardModel->getaugLost();
+			$augPros= $this->dashboardModel->getaugPros();
+			$augNeg= $this->dashboardModel->getaugNeg();
+			$sepWon= $this->dashboardModel->getsepWon();
+			$sepLost= $this->dashboardModel->getsepLost();
+			$sepPros= $this->dashboardModel->getsepPros();
+			$sepNeg= $this->dashboardModel->getsepNeg();
+			
+					
+			
+			$this->view('dashboards/displayDashboards', $data, $datas, $data1, $data2, $data3, $data4, $data5, $data6, $janWon, $janLost, $janPros, $janNeg,$febWon, $febLost, $febPros, $febNeg,$marWon, $marLost, $marPros, $marNeg,$aprWon, $aprLost, $aprPros, $aprNeg,$mayWon, $mayLost, $mayPros, $mayNeg,$junWon, $junLost, $junPros, $junNeg,$julWon, $julLost, $julPros, $julNeg,$augWon, $augLost, $augPros, $augNeg,$sepWon, $sepLost, $sepPros, $sepNeg);
         }
     }
 
--- app/models/Dashboard.php
+++ app/models/Dashboard.php
@@ -69,6 +69,260 @@ class Dashboard{
         
         return $result;
     }
+	
+	public function getjanWon(){
+	    $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-01-%"');
+	    
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+	public function getjanLost(){
+	    $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-01-%"');
+	    
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+	public function getjanPros(){
+	    $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-01-%"');
+	    
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+	public function getjanNeg(){
+	    $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-01-%"');
+	    
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getfebWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-02-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getfebLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-02-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getfebPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-02-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getfebNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-02-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmarWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-03-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmarLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-03-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmarPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-03-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmarNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-03-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaprWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-04-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaprLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-04-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaprPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-04-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaprNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-04-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmayWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-05-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmayLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-05-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmayPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-05-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getmayNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-05-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjunWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-06-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjunLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-06-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjunPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-06-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjunNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-06-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjulWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-07-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjulLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-07-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjulPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-07-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getjulNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-07-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaugWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-08-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaugLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-08-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaugPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-08-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getaugNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-08-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getsepWon(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Won" AND opportunities.date LIKE "2021-09-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getsepLost(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Lost" AND opportunities.date LIKE "2021-09-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getsepPros(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Prospecting" AND opportunities.date LIKE "2021-09-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    public function getsepNeg(){
+        $this->db->query('SELECT COUNT(opportunities.id) FROM opportunities INNER JOIN stages ON stages.id=opportunities.stage_id WHERE stages.name="Negotiation" AND opportunities.date LIKE "2021-09-%"');
+        
+        $result = $this->db->resultSet();
+        
+        return $result;
+    }
+    
     
     
     
